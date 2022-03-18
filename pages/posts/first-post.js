cb8225df1ee4bd059d159965ccfeca06e475fb78
Aidan <aidan.lowson@contactpartners.com>
2022-03-18 15:59:01 +0000

    Adding styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,26 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+
+import Layout from '../../components/Layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() => console.log('FB Script loaded')}
+        />
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href='/'>
           <a>Back home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
